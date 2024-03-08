260dfb7833547f50cf6195b05463f3ff7923ba01
DmitryLanin <dmitrylanin@gmail.com>
2024-03-08 23:32:18 +0300

    Added workers to List

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Worker w1 = new Worker("S", 39, 390, Position.SENIOR.getPosition());
@@ -6,5 +9,7 @@ public class Main {
         Worker w4 = new Worker("M", 33, 330, Position.JUNIOR.getPosition());
         Worker w5 = new Worker("G", 38, 380, Position.SENIOR.getPosition());
         Worker w6 = new Worker("E", 44, 440, Position.SENIOR.getPosition());
+
+        List<Worker> workers = Arrays.asList(w1, w2, w3, w4, w5, w6);
     }
 }
--- src/StreamEngine.java
+++ src/StreamEngine.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
