b3dbdbe6b4f7d02d3c59d3f959c8621d7d38fae4
DmitryLanin <dmitrylanin@gmail.com>
2024-03-08 23:38:18 +0300

    Added new worker

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Worker w4 = new Worker("M", 33, 330, Position.JUNIOR.getPosition());
         Worker w5 = new Worker("G", 38, 380, Position.SENIOR.getPosition());
         Worker w6 = new Worker("E", 44, 440, Position.SENIOR.getPosition());
+        Worker w7 = new Worker("Z", 24, 240, Position.JUNIOR.getPosition());
 
         List<Worker> workers = Arrays.asList(w1, w2, w3, w4, w5, w6);
     }
