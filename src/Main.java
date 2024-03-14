bfea3530d73d31474bef23f7bf53dfaf0e0989f6
DmitryLanin <dmitrylanin@gmail.com>
2024-03-14 13:13:27 +0300

    Delete some workers

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<Worker> workers = getWorkers();
+
+        Map<String, List<Worker>> map1 = workers.stream()
+                .collect(Collectors.groupingBy(Worker::getPosition));
+        map1.entrySet().stream().forEach(e-> System.out.println(e));
+    }
+
+    private static List<Worker> getWorkers() {
         Worker w1 = new Worker("Ss", 39, 390, Position.SENIOR.getPosition());
         Worker w2 = new Worker("Rr", 45, 450, Position.MIDDLE.getPosition());
         Worker w3 = new Worker("Aa", 41, 410, Position.MIDDLE.getPosition());
-        Worker w4 = new Worker("Mm", 33, 330, Position.JUNIOR.getPosition());
-        Worker w5 = new Worker("Gg", 38, 380, Position.SENIOR.getPosition());
-        Worker w6 = new Worker("Ee", 44, 440, Position.SENIOR.getPosition());
-        Worker w7 = new Worker("Zz", 24, 240, Position.JUNIOR.getPosition());
 
-        List<Worker> workers = Arrays.asList(w1, w2, w3, w4, w5, w6);
+        List<Worker> workers = Arrays.asList(w1, w2, w3);
+        return workers;
     }
 }
